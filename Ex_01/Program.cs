d46ebcce2b0bdd32eb729d296b7e5d81d0c6a9ee
«Svetlana <svdorohova@gmail.com>
2022-09-05 21:30:55 +0300

    Первое дз

---
+++ Ex_01/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//нельзя использовать pow
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+int res(int x, int y)
+{
+    int result = x;
+    for (int i = 1; i < y; i++)
+    {
+        result = result * x;
+    }
+    return result;
+
+}
+System.Console.WriteLine("Введите числа через запятую");
+string first = Console.ReadLine();
+first = first.Replace(" ", "");
+string[] mas = first.Split(",");
+string a = mas[0];
+string b = mas[1];
+int a1 = Convert.ToInt32(a);
+int b2 = Convert.ToInt32(b);
+
+System.Console.WriteLine(res(a1, b2));
