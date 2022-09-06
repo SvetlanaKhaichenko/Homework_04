88b6ba2560327afca1d3f576cca11925ce2112df
«Svetlana <svdorohova@gmail.com>
2022-09-06 16:23:16 +0300

    Add Ex03

---
+++ Ex_03/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите метод, который задаёт массив из 8 элементов (диапазон от 1 до 100) и выводит их на экран.
+//пример вывода
+
+int[] RandomMas()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 101);
+    }
+    return array;
+}
+
+void PrintMas(int[] Mas)
+{
+    for (int i = 0; i < 1; i++)
+    {
+        System.Console.Write($"[{Mas[i]}, ");
+        for (int j = 1; j < Mas.Length - 1; j++)
+        {
+            System.Console.Write($"{Mas[j]}, ");
+        }
+        for (int k = Mas.Length - 1; k < Mas.Length; k++)
+        {
+            System.Console.Write($"{Mas[k]}]");
+        }
+    }
+}
+
+PrintMas(RandomMas());
