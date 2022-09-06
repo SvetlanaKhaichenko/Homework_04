0a0f4c3740fedb57704c8354bb997a0f42f75722
«Svetlana <svdorohova@gmail.com>
2022-09-06 16:05:18 +0300

    Add Ex02

---
+++ Ex_02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+int Sum(int value)
+{
+    int count = 0;
+    if (value < 10) return value;
+    else
+        while (value > 10)
+        {
+            int b = value % 10;
+            value = value / 10;
+            count = count + b;
+        }
+    return count = count + value;
+
+}
+
+System.Console.WriteLine("Введите число");
+int valuePolz = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Sum(valuePolz));
+
+
+
+
